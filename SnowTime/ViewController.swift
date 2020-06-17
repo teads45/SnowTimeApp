68a235585f9fa9b8e40454187f55946f02b0366d
Thomas Eads <eadsthomas45@gmail.com>
2020-06-17 13:00:04 -0400

    Initial Commit

---
+++ SnowTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowTime
+//
+//  Created by Thomas Eads on 6/17/20.
+//  Copyright © 2020 Thomas Eads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
