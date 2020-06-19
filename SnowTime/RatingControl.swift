3e01571d219dbd5dd5e7662aba2de2a6019c2766
Thomas Eads <eadsthomas45@gmail.com>
2020-06-18 22:06:15 -0400

    New corrected code with better documentation

---
+++ SnowTime/RatingControl.swift
@@ -0,0 +1,48 @@
+//
+//  RatingControl.swift
+//  SnowTime
+//
+//  Created by Thomas Eads on 6/18/20.
+//  Copyright © 2020 Thomas Eads. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+//MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        setupButtons()
+    }
+        required init(coder:NSCoder){
+            super.init(coder:coder)
+            setupButtons()
+        }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons(){
+        
+        //create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
+}
+
+
---
+++ SnowTime/viewController2.swift
@@ -0,0 +1,22 @@
+//
+//  viewController2.swift
+//  SnowTime
+//
+//  Created by Thomas Eads on 6/17/20.
+//  Copyright © 2020 Thomas Eads. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class viewControllerTwo : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func Settings(_ sender: UIButton) {
+    }
+    
+    
+    
+}
