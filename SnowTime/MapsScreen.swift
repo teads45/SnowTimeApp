c60c22d8030e100914c99b37cd69ec345d5f328b
thomaseads <thomastepheneads@gmail.com>
2020-06-22 12:50:36 -0400

    Updated maps etc.

--- SnowTime/MapsScreen.swift
+++ SnowTime/MapsScreen.swift
@@ -8,18 +8,39 @@
 
 import Foundation
 import UIKit
+import MapKit
 
-class mapsScreen : UIViewController{
+
+class lodgesMapPage : UIViewController, MKMapViewDelegate {
+    
     
     //MARK: Properties
-    @IBOutlet weak var mapsSpecificationLabel: UITextField!
+   // @IBOutlet var mapsSpecificationLabel: UILabel!
+    @IBOutlet weak var matchingLodgesShown: UILabel!
     
+    @IBOutlet weak var lodgesMap: MKMapView!
+    
+   
+    @IBOutlet weak var goBackButton: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }/*
-//see if the device can handle maps properly
+        let skiLodge = CLLocation(latitude: 40.7608, longitude: 111.8910)
+        let regionRadius: CLLocationDistance = 1000.0
+        let region = MKCoordinateRegion(center: skiLodge.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        lodgesMap.setRegion(region, animated: false)
+        lodgesMap.delegate = self
+        
+    }
+    
+    
+    
+    //Thread 1: Exception: "[<UIViewController 0x7fed95c0d680> setValue:forUndefinedKey:]: this class is not key value coding-compliant for the key lodgesMap."
+    
+    
+    /*
+//see if the device can handle maps properly (check if device has hardware to support)
     class func significantLocationChangeMonitoringAvailable() -> Bool{
         if  = true{
             //tell maps it works
@@ -35,4 +56,10 @@ class mapsScreen : UIViewController{
     }
         
         */
+    
+}
+extension MKMapView: MKMapViewDelegate{
+    public func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+        print("Map rendering...")
+    }
 }
