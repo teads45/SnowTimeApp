4fd4400c3eea8af6e7d8ea4f6135fed6b6a1cf4a
Thomas Eads <eadsthomas45@gmail.com>
2020-07-07 17:31:07 -0400

    server works to call to YelpFusion

--- SnowTime/yelpReview.swift
+++ SnowTime/yelpReview.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 
-class yelpTableViewController: UITableViewController{
+class yelpTableViewController: UIViewController {
     
+    
+   // @IBOutlet weak var lodgesTableView: UITableView!
     @IBOutlet weak var lodgesTableView: UITableView!
     
     //coordinates for SLC, Utah
-    let CPLatitude: Double = 40.1
-    let CPLongitude: Double = -111.1
+    let CPLatitude: Double = 40.744797
+    let CPLongitude: Double = -111.928734
     
     var lodges: [Lodges] = []
     
@@ -30,84 +32,41 @@ class yelpTableViewController: UITableViewController{
         
         
         
-        retrieveLodges(latitude: CPLatitude, longitude: CPLongitude, limit: 10, sortBy: "distance", locale: "en_US", category: "ski Lodges") { (response, error) in
+        retrieveLodges(latitude: CPLatitude, longitude: CPLongitude, limit: 10, sortBy: "distance", locale: "en_US", category: "Ski_Resorts") { (response, error) in
             
             if let response = response {
                 self.lodges = response
-            
-                }
-            }
-        }
-    func retrieveLodges(latitude: Double,
-                            longitude: Double,
-                            limit: Int,
-                            sortBy: String,
-                            locale: String,
-                            category: String,
-                            completionHandler: @escaping ([Lodges]?, Error?) -> Void){
-            //inset api key from yelp
-            let apikey = "thbwyY1lg0-9YAYhUpKYDlSgpJhJkrZQgQv1VYUJkDljgZUlw-wKU9i_nEUeNLwF01n8S4IovCt8vTWzi1iTdjdvhJemzapfWmEWLLPhdfxbu64YXMgOGFTO8IrzXnYx"
-
-            // insert base URL for search
-            let baseURL = "https://api.yelp.com/v3/businesses/search"
-            let url = URL(string: baseURL)
-        
-         var request = URLRequest(url:url!)
-            
-            request.setValue("Bearer\(apikey)", forHTTPHeaderField: "Authorization")
-                request.httpMethod = "GET"
-
-
-
-                //Initialize session and task
-                URLSession.shared.dataTask(with: request) {(data, response, error)in
-                if let error = error {
-                    completionHandler(nil, error)
-                    
-                }
-                
-                do {
-                    //read data as JSON
-                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                    
-                    //main dictionary
-                    guard let resp = json as? NSDictionary else {return}
-                    
-                    //businesses
-                    guard let businesses = resp.value(forKey: "businesses") as? [NSDictionary] else {return}
-                    
-                    var lodgesList: [Lodges] = []
-                    
-                    // accessing each business
-                    for business in businesses {
-                        var lodge = Lodges()
-                        lodge.name = business.value(forKey: "name") as? String
-                        lodge.price = business.value(forKey: "price") as? String
-                        lodge.rating = business.value(forKey: "rating") as? Float
-                        lodge.distance = business.value(forKey: "distance") as? Double
-                        let address = business.value(forKeyPath: "location.dispay_address") as? [String]
-                        lodge.address = address?.joined(separator: "\n")
-                        
-                        lodgesList.append(lodge)
+                DispatchQueue.main.async{
+                    self.lodgesTableView.reloadData()
                     }
-                    
-                    completionHandler(lodgesList, nil)
-                } catch {
-                    print("caught error")
-                    }
-                }.resume()
-            }
+                }
         }
-
-   
-
-
-
-
+    }
+}
+  
 
 extension yelpTableViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomCell
+        cell.nameLabel.text = lodges[indexPath.row].name
+        cell.ratingLabel.text = String(lodges[indexPath.row].rating ?? 0.0)
+        cell.priceLabel.text = lodges[indexPath.row].price ?? "-"
+        cell.addressLabel.text = lodges[indexPath.row].address
+        return cell
+           
+       }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return lodges.count
+        
+    }
+    
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return 300
+           
+       }
+    
+     
 }
+
    /* func setStarNumber(_ selected: false, animated: true) {
         super.setSelected(selected, animated: animated)
 
