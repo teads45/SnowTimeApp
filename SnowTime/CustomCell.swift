77b517af41ed87b256095f5c9a33536803217d68
Thomas Eads <eadsthomas45@gmail.com>
2020-07-07 15:46:23 -0400

    commit bc my computer is low on battery (server set up in the works)

---
+++ SnowTime/CustomCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomCell.swift
+//  SnowTime
+//
+//  Created by Thomas Eads on 7/7/20.
+//  Copyright © 2020 Thomas Eads. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var parentView:UIView!
+    @IBOutlet weak var nameLabel:UILabel!
+    @IBOutlet weak var ratingLabel:UILabel!
+    @IBOutlet weak var priceLabel:UILabel!
+    @IBOutlet weak var distanceLabel:UILabel!
+    @IBOutlet weak var addressLabel:UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        parentView.layer.cornerRadius = 15
+    }
+
+
+}
---
+++ SnowTime/Lodges.swift
@@ -0,0 +1,20 @@
+//
+//  Lodges.swift
+//  SnowTime
+//
+//  Created by Thomas Eads on 7/7/20.
+//  Copyright © 2020 Thomas Eads. All rights reserved.
+//equal to retrieve venues
+
+import Foundation
+
+struct Lodges{
+
+var name    :String?
+var rating  :Float?
+var price   :String?
+var distance    :Double?
+var address     :String?
+    
+}
+
